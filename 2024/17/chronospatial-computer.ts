02013f7830141226ec996685b6e6145d90352da7
Cameron Sanchez <cameron.sanchez@chghealthcare.com>
2024-12-23 00:35:29 -0700

    day 17 part 2 naive

--- 2024/17/chronospatial-computer.ts
+++ 2024/17/chronospatial-computer.ts
@@ -3,32 +3,23 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
-const input = readFileSync(resolve(__dirname, 'input'), 'utf-8');
 const registerIds = ['A', 'B', 'C'] as const;
 type RegisterId = (typeof registerIds)[number];
 
-function getRegisterValue(id: RegisterId): number {
+function getRegisterValue(input: string, id: RegisterId): number {
   const match = new RegExp(`Register\\s+${id}:\\s+(\\d+)`).exec(input);
   return match ? parseInt(match[1], 10) : 0;
 }
 
-const registers = registerIds.reduce(
-  (record, id) => {
-    record[id] = getRegisterValue(id);
-    return record;
-  },
-  {
-    A: 0,
-    B: 0,
-    C: 0,
-  },
-);
+function getProgram(input: string): string {
+  const [match] = /Program:\s+(\d,?)+/.exec(input) ?? [''];
+  const [, program] = match.split(/\s+/);
+  return program;
+}
 
 type Instruction = [number, number];
 
-function getInstructions(): Instruction[] {
-  const [match] = /Program:\s+(\d,?)+/.exec(input) ?? [''];
-  const [, program] = match.split(/\s+/);
+function getInstructions(program: string): Instruction[] {
   const values = program.split(',').map((value) => parseInt(value, 10));
   const instructions: Instruction[] = [];
 
@@ -40,68 +31,103 @@ function getInstructions(): Instruction[] {
   return instructions;
 }
 
-const instructions: Instruction[] = getInstructions();
-const output: number[] = [];
-let index = 0;
-let jumped = false;
-
-function getCombo(operand: number): number {
-  if (operand === 4) {
-    return registers.A;
-  } else if (operand === 5) {
-    return registers.B;
-  } else if (operand === 6) {
-    return registers.C;
-  }
-
-  return operand;
+interface Program {
+  registers: Record<RegisterId, number>;
+  instructions: Instruction[];
 }
 
-function getDivision(operand: number): number {
-  return Math.floor(registers.A / 2 ** getCombo(operand));
-}
+function run(program: Program): string {
+  const { registers, instructions } = program;
+  const output: number[] = [];
+  let index = 0;
+  let jumped = false;
 
-type Operation = (operand: number) => void;
-
-const operations: Record<number, Operation> = {
-  0: (operand) => {
-    registers.A = getDivision(operand);
-  },
-  1: (operand) => {
-    registers.B = registers.B ^ operand;
-  },
-  2: (operand) => {
-    registers.B = getCombo(operand) % 8;
-  },
-  3: (operand) => {
-    if (registers.A !== 0) {
-      index = Math.floor(operand / 2);
-      jumped = true;
+  function getCombo(operand: number): number {
+    if (operand === 4) {
+      return registers.A;
+    } else if (operand === 5) {
+      return registers.B;
+    } else if (operand === 6) {
+      return registers.C;
     }
-  },
-  4: () => {
-    registers.B = registers.B ^ registers.C;
-  },
-  5: (operand) => {
-    output.push(getCombo(operand) % 8);
-  },
-  6: (operand) => {
-    registers.B = getDivision(operand);
-  },
-  7: (operand) => {
-    registers.C = getDivision(operand);
-  },
-};
 
-while (index < instructions.length) {
-  const [opcode, operand] = instructions[index];
-  operations[opcode](operand);
-
-  if (!jumped) {
-    index++;
-  } else {
-    jumped = false;
+    return operand;
   }
+
+  function getDivision(operand: number): number {
+    return Math.floor(registers.A / 2 ** getCombo(operand));
+  }
+
+  type Operation = (operand: number) => void;
+
+  const operations: Record<number, Operation> = {
+    0: (operand) => {
+      registers.A = getDivision(operand);
+    },
+    1: (operand) => {
+      registers.B = registers.B ^ operand;
+    },
+    2: (operand) => {
+      registers.B = getCombo(operand) % 8;
+    },
+    3: (operand) => {
+      if (registers.A !== 0) {
+        index = Math.floor(operand / 2);
+        jumped = true;
+      }
+    },
+    4: () => {
+      registers.B = registers.B ^ registers.C;
+    },
+    5: (operand) => {
+      output.push(getCombo(operand) % 8);
+    },
+    6: (operand) => {
+      registers.B = getDivision(operand);
+    },
+    7: (operand) => {
+      registers.C = getDivision(operand);
+    },
+  };
+
+  while (index < instructions.length) {
+    const [opcode, operand] = instructions[index];
+    operations[opcode](operand);
+
+    if (!jumped) {
+      index++;
+    } else {
+      jumped = false;
+    }
+  }
+
+  return output.join(',');
 }
 
-console.log(output.join(','));
+const input = readFileSync(resolve(__dirname, 'input'), 'utf-8');
+
+const registers = registerIds.reduce(
+  (record, id) => {
+    record[id] = getRegisterValue(input, id);
+    return record;
+  },
+  {
+    A: 0,
+    B: 0,
+    C: 0,
+  },
+);
+
+const programString = getProgram(input);
+const instructions: Instruction[] = getInstructions(programString);
+const program = { registers, instructions };
+let output = run(program);
+console.log(output);
+let A = 0;
+
+while (output !== programString) {
+  A++;
+  output = run({ ...program, registers: { ...registers, A } });
+}
+
+console.log(A);
