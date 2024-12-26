5bd1750020d13cc6df3bc232071c9735fabb44e4
Cameron Sanchez <cameron.sanchez@chghealthcare.com>
2024-12-26 15:36:41 -0700

    day 21 optimized part 2

--- 2024/21/keypad-conundrum.ts
+++ 2024/21/keypad-conundrum.ts
@@ -10,129 +10,185 @@ interface Path {
   visited: Set<string>;
 }
 
-function enter(keypad: string[], code: string): string[] {
-  let combinations: string[] = [];
-  let start = toCoordinate(keypad[0].length, keypad.join('').indexOf('A'));
+function getOptimalSequences(keypad: string[]): Record<string, string[]> {
+  const buttons = keypad.join('').replace(' ', '').split('');
+  const pairs = new Set<string>();
 
-  code.split('').forEach((button) => {
-    let sequences: string[] = [];
+  buttons.forEach((a) => {
+    buttons.forEach((b) => {
+      pairs.add(`${a}-${b}`);
+      pairs.add(`${b}-${a}`);
+    });
+  });
 
-    let paths: Path[] = [
-      { position: start, sequence: [], visited: new Set([' ']) },
-    ];
+  const pairTuples = [...pairs].map((pair) => pair.split('-'));
 
-    while (paths.length) {
-      const newPaths: Path[] = [];
+  return pairTuples.reduce(
+    (optimalSequences: Record<string, string[]>, [a, b]) => {
+      const start = toCoordinate(keypad[0].length, keypad.join('').indexOf(a));
+      let sequences: string[] = [];
 
-      paths.forEach((path) => {
-        if (keypad[path.position[0]][path.position[1]] === button) {
-          start = path.position;
-          const newSequence = [...path.sequence, 'A'].join('');
+      let paths: Path[] = [
+        { position: start, sequence: [], visited: new Set([' ']) },
+      ];
 
-          if (!sequences.length || newSequence.length < sequences[0].length) {
-            sequences = [newSequence];
-          } else if (newSequence.length === sequences[0].length) {
-            sequences.push(newSequence);
+      while (paths.length) {
+        const newPaths: Path[] = [];
+
+        paths.forEach((path) => {
+          if (keypad[path.position[0]][path.position[1]] === b) {
+            const newSequence = [...path.sequence, 'A'].join('');
+
+            if (!sequences.length || newSequence.length < sequences[0].length) {
+              sequences = [newSequence];
+            } else if (newSequence.length === sequences[0].length) {
+              sequences.push(newSequence);
+            }
+          }
+
+          const [row, col] = path.position;
+
+          if (row > 0 && !path.visited.has(keypad[row - 1][col])) {
+            newPaths.push({
+              position: [row - 1, col],
+              sequence: [...path.sequence, '^'],
+              visited: new Set([...path.visited, keypad[row][col]]),
+            });
+          }
+
+          if (
+            row < keypad.length - 1 &&
+            !path.visited.has(keypad[row + 1][col])
+          ) {
+            newPaths.push({
+              position: [row + 1, col],
+              sequence: [...path.sequence, 'v'],
+              visited: new Set([...path.visited, keypad[row][col]]),
+            });
+          }
+
+          if (col > 0 && !path.visited.has(keypad[row][col - 1])) {
+            newPaths.push({
+              position: [row, col - 1],
+              sequence: [...path.sequence, '<'],
+              visited: new Set([...path.visited, keypad[row][col]]),
+            });
+          }
+
+          if (
+            col < keypad[0].length - 1 &&
+            !path.visited.has(keypad[row][col + 1])
+          ) {
+            newPaths.push({
+              position: [row, col + 1],
+              sequence: [...path.sequence, '>'],
+              visited: new Set([...path.visited, keypad[row][col]]),
+            });
+          }
+        });
+
+        paths = newPaths;
+      }
+
+      optimalSequences[`${a}-${b}`] = sequences.filter((sequence) => {
+        let direction = sequence[0];
+        let turned = false;
+
+        for (const character of sequence.slice(0, sequence.length - 1)) {
+          if (character !== direction && turned) {
+            return false;
+          } else if (character !== direction) {
+            direction = character;
+            turned = true;
           }
         }
 
-        const [row, col] = path.position;
-
-        if (row > 0 && !path.visited.has(keypad[row - 1][col])) {
-          newPaths.push({
-            position: [row - 1, col],
-            sequence: [...path.sequence, '^'],
-            visited: new Set([...path.visited, keypad[row][col]]),
-          });
-        }
-
-        if (
-          row < keypad.length - 1 &&
-          !path.visited.has(keypad[row + 1][col])
-        ) {
-          newPaths.push({
-            position: [row + 1, col],
-            sequence: [...path.sequence, 'v'],
-            visited: new Set([...path.visited, keypad[row][col]]),
-          });
-        }
-
-        if (col > 0 && !path.visited.has(keypad[row][col - 1])) {
-          newPaths.push({
-            position: [row, col - 1],
-            sequence: [...path.sequence, '<'],
-            visited: new Set([...path.visited, keypad[row][col]]),
-          });
-        }
-
-        if (
-          col < keypad[0].length - 1 &&
-          !path.visited.has(keypad[row][col + 1])
-        ) {
-          newPaths.push({
-            position: [row, col + 1],
-            sequence: [...path.sequence, '>'],
-            visited: new Set([...path.visited, keypad[row][col]]),
-          });
-        }
+        return true;
       });
 
-      paths = newPaths;
+      return optimalSequences;
+    },
+    {},
+  );
+}
+
+interface Robot {
+  cache: Record<string, number>;
+  key: string;
+  keypad: Record<string, string[]>;
+}
+
+function createRobot(keypad: Record<string, string[]>): Robot {
+  return {
+    cache: {},
+    key: 'A',
+    keypad,
+  };
+}
+
+interface ChainParams {
+  indirection: number;
+  keypads: {
+    directional: Record<string, string[]>;
+    numeric: Record<string, string[]>;
+  };
+}
+
+function createChain({ keypads, indirection }: ChainParams): Robot[] {
+  return [
+    createRobot(keypads.numeric),
+    ...Array.from<unknown, Robot>({ length: indirection }, () =>
+      createRobot(keypads.directional),
+    ),
+  ];
+}
+
+function calculateCost(robots: Robot[], code: string): number {
+  const [robot, ...remaining] = robots;
+
+  return code.split('').reduce((costSoFar, button) => {
+    const cacheKey = `${robot.key}-${button}`;
+    robot.key = button;
+
+    if (robot.cache[cacheKey]) {
+      return costSoFar + robot.cache[cacheKey];
     }
 
-    combinations = combinations.length
-      ? combinations.flatMap((combination) =>
-          sequences.map((sequence) => `${combination}${sequence}`),
-        )
-      : sequences;
-  });
+    const sequences = robot.keypad[cacheKey];
 
-  return combinations;
+    const cost = remaining.length
+      ? Math.min(
+          ...sequences.map((sequence) => calculateCost(remaining, sequence)),
+        )
+      : sequences[0].length;
+
+    robot.cache[cacheKey] = cost;
+    return costSoFar + cost;
+  }, 0);
 }
 
 function extractNumeric(code: string): number {
   return parseInt(code.replaceAll(/\D/g, ''), 10);
 }
 
-function calculateComplexity(keypads: string[][], code: string): number {
-  const shortestSequences = keypads.reduce(
-    (sequences, keypad) => {
-      const newSequences = sequences.flatMap((sequence) =>
-        enter(keypad, sequence),
-      );
-
-      const newShortest = newSequences.reduce(
-        (shortest, current) =>
-          current.length < shortest ? current.length : shortest,
-        Number.POSITIVE_INFINITY,
-      );
-
-      return newSequences.filter(({ length }) => length === newShortest);
-    },
-    [code],
-  );
-
-  return shortestSequences[0].length * extractNumeric(code);
+function calculateComplexity(robots: Robot[], code: string): number {
+  return calculateCost(robots, code) * extractNumeric(code);
 }
 
-function sumComplexities(keypads: string[][], codes: string[]): number {
+function sumComplexities(robots: Robot[], codes: string[]): number {
   return codes.reduce(
-    (sum, code) => sum + calculateComplexity(keypads, code),
+    (sum, code) => sum + calculateComplexity(robots, code),
     0,
   );
 }
 
-const numericKeypad = ['789', '456', '123', ' 0A'];
-const directionalKeypad = [' ^A', '<v>'];
+const keypads = {
+  directional: getOptimalSequences([' ^A', '<v>']),
+  numeric: getOptimalSequences(['789', '456', '123', ' 0A']),
+};
+
 const input = readFileSync(resolve(__dirname, 'input'), 'utf-8');
 const codes = input.split('\n');
-
-console.log(
-  sumComplexities([numericKeypad, directionalKeypad, directionalKeypad], codes),
-);
-
-const directionals = Array.from<string[]>({ length: 25 }).fill(
-  directionalKeypad,
-);
-
-console.log(sumComplexities([numericKeypad, ...directionals], codes));
+const chain = createChain({ indirection: 2, keypads });
+console.log(sumComplexities(chain, codes));
+console.log(sumComplexities(createChain({ indirection: 25, keypads }), codes));
